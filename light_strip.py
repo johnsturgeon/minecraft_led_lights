627f2f3dc938210305e8d126bae986cf7d5a832f
John Sturgeon <john.sturgeon@me.com>
2022-01-09 12:03:48 -0800

    Update flask to be visible outside the network

---
+++ light_strip.py
@@ -0,0 +1,18 @@
+import time
+import board
+import neopixel
+
+pixel_pin = board.D18
+num_pixels = 300
+ORDER = neopixel.GRB
+
+pixels = neopixel.NeoPixel(
+    pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
+)
+
+def blink_pixel(pixel_number: int):
+    pixels[pixel_number] = (255, 255, 255)
+    pixels.show()
+    time.sleep(.1)
+    pixels[pixel_number] = (0, 0, 0)
+    pixels.show()
